3daa488a41a4d576d7b4d86aee2b7871a3991611
Aparnastupid <118095865+Aparnastupid@users.noreply.github.com>
2022-11-21 10:51:31 +0530

    Create caseif.java

---
+++ 1.1/caseif.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package practice;
+
+/**
+ * @author APARNA
+ *
+ */
+public class caseif {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1,j=2,c=i+j;
+		
+        if(i<=j)
+        { System.out.println("TRUE\n"+ c);
+        }
+        
+        else if (i==j)
+        {
+        	 System.out.println("FALSE");
+        	
+        	
+        	 
+}
+	}
+
+}
